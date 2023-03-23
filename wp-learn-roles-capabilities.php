5985eddb9089f49bcd4eefe04216e04374cd6b6d
Jonathan Bossenger <jonathanbossenger@gmail.com>
2023-03-23 11:24:48 +0200

    Updated books to stories

--- wp-learn-roles-capabilities.php
+++ wp-learn-roles-capabilities.php
@@ -20,43 +20,39 @@ function wp_learn_submenu() {
 
 	add_submenu_page(
 		'tools.php',
-		esc_html__( 'WP Learn Book CPT', 'wp_learn' ),
-		esc_html__( 'WP Learn Book CPT', 'wp_learn' ),
+		esc_html__( 'WP Learn Story CPT', 'wp_learn' ),
+		esc_html__( 'WP Learn Story CPT', 'wp_learn' ),
 		'manage_options',
-		'wp_learn_book_cpt',
-		'wp_learn_render_book_cpt'
+		'wp_learn_story_cpt',
+		'wp_learn_render_story_cpt'
 	);
-
 }
 
 /**
  * Render the Role admin page
  */
 function wp_learn_render_admin_page() {
-	$editor_role = get_role( 'editor' );
-	$assistant_role = get_role( 'assistant' );
+	$author_role = get_role( 'author' );
 	?>
     <div class="wrap" id="wp_learn_admin">
         <h1>Roles</h1>
-        <pre style="font-size: 22px; line-height: 1.2;"><?php print_r( $editor_role ) ?></pre>
-        <pre style="font-size: 22px; line-height: 1.2;"><?php print_r( $assistant_role ) ?></pre>
+        <pre style="font-size: 22px; line-height: 1.2;"><?php print_r( $author_role  ) ?></pre>
     </div>
 	<?php
 }
 
 /**
- * Render the Role admin page
+ * Render the Story admin page
  */
-function wp_learn_render_book_cpt() {
-	$book = $GLOBALS['wp_post_types']['book']
-
+function wp_learn_render_story_cpt() {
+	$story = $GLOBALS['wp_post_types']['story']
 	?>
     <div class="wrap" id="wp_learn_admin">
         <h1>Book</h1>
-        <pre style="font-size: 22px; line-height: 1.2;"><?php print_r( array( 'capability_type' => $book->capability_type ) ) ?></pre>
-        <pre style="font-size: 22px; line-height: 1.2;"><?php print_r( array( 'map_meta_cap' => $book->map_meta_cap ) ) ?></pre>
-        <pre style="font-size: 22px; line-height: 1.2;"><?php print_r( array( 'cap' => $book->cap ) ) ?></pre>
-        <pre style="font-size: 22px; line-height: 1.2;"><?php print_r( $book ) ?></pre>
+        <pre style="font-size: 22px; line-height: 1.2;"><?php print_r( array( 'capability_type' => $story->capability_type ) ) ?></pre>
+        <pre style="font-size: 22px; line-height: 1.2;"><?php print_r( array( 'map_meta_cap' => $story->map_meta_cap ) ) ?></pre>
+        <pre style="font-size: 22px; line-height: 1.2;"><?php print_r( array( 'cap' => $story->cap ) ) ?></pre>
+        <pre style="font-size: 22px; line-height: 1.2;"><?php print_r( $story ) ?></pre>
     </div>
 	<?php
 }
@@ -108,14 +104,14 @@ function wp_learn_remove_custom_role() {
 add_action( 'init', 'wp_learn_init' );
 function wp_learn_init() {
 	/**
-	 * Register a book custom post type
+	 * Register a story custom post type
 	 */
 	register_post_type(
-		'book',
+		'story',
 		array(
 			'labels'          => array(
-				'name'          => __( 'Books' ),
-				'singular_name' => __( 'Book' )
+				'name'          => __( 'Stories' ),
+				'singular_name' => __( 'Story' )
 			),
 			'public'          => true,
 			'show_ui'         => true,
